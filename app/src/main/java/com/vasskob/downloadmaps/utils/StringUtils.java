b6f3dc430b5bfa230b786840b893c0e6bc71b432
vasskob <vasskob@gmail.com>
2018-01-26 17:42:44 +0200

    String utils/resource refactoring

--- app/src/main/java/com/vasskob/downloadmaps/utils/StringUtils.java
+++ app/src/main/java/com/vasskob/downloadmaps/utils/StringUtils.java
@@ -1,36 +1,25 @@
 package com.vasskob.downloadmaps.utils;
 
-import android.text.TextUtils;
-
 import com.vasskob.downloadmaps.BuildConfig;
-import com.vasskob.downloadmaps.domain.model.Region;
 
 public class StringUtils {
+
+    private static final String QUERY = "download.php?standard=yes&file=";
+    private static String FILE_URL_PREFIX = BuildConfig.BASE_URL + QUERY;
+    private static final String FILE_URL_SUFFIX = "_2.obf.zip";
+
+    public static String getDownloadURL(String regionName) {
+        return FILE_URL_PREFIX + getCapitalName(regionName) + FILE_URL_SUFFIX;
+    }
 
     public static String getCapitalName(String name) {
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
-    public static String getDownloadURL(Region region) {
-        if (TextUtils.isEmpty(region.getParent().getName())) {
-            return BuildConfig.BASE_URL + "download.php?standard=yes&file="
-                    + getCapitalName(region.getName())
-                    + "_2.obf.zip";
-        } else if (TextUtils.isEmpty(region.getParent().getParent().getName())) {
-            return BuildConfig.BASE_URL + "download.php?standard=yes&file="
-                    + getCapitalName(region.getName())
-                    + "_"
-                    + region.getParent().getName()
-                    + "_2.obf.zip";
-        } else return BuildConfig.BASE_URL + "download.php?standard=yes&file="
-                + getCapitalName(region.getParent().getName())
-                + "_"
-                + region.getName()
-                + "_"
-                + region.getParent().getParent().getName()
-                + "_2.obf.zip";
+    public static String getFileName(String fileUrl) {
+        return fileUrl.substring(FILE_URL_PREFIX.length(), fileUrl.length());
     }
 }
 /*
-http://download.osmand.net/download.php?standard=yes&file=Berlin_germany_europe_2.obf.zip
-http://download.osmand.net/download.php?standard=yes&file=Germany_berlin_europe_2.obf.zip TRUE*/
+http://download.osmand.net/download.php?standard=yes&file=Berlin_germany_europe_2.obf.zip Bad
+http://download.osmand.net/download.php?standard=yes&file=Germany_berlin_europe_2.obf.zip Good */
